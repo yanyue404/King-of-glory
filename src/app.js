a99aec20c0c1dfb024393438d25a338bdbb1c9e9
yanyue404 <xiaoyueyue165@gmail.com>
2025-05-22 16:59:28 +0800

    feat: 添加 vercel 用来部署

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 'use esversion：6';
+
 const http = require('http');
 const Router = require('./router');
 const bindRender = require('./utils/utils.js');
@@ -9,7 +10,18 @@ server.on('request', (req, res) => {
   Router(req, res);
 });
 
-server.listen(3000, function() {
-  console.log('Run in localhost:3000 ');
-});
+// 本地开发时使用
+if (process.env.NODE_ENV !== 'production') {
+  server.listen(3000, function() {
+    console.log('Run in localhost:3000 ');
+  });
+}
+
+// 为了保持测试兼容性，导出服务器对象
 module.exports = server;
+
+// 为 Vercel 导出处理函数
+module.exports.handler = (req, res) => {
+  bindRender(res);
+  Router(req, res);
+};
--- src/router.js
+++ src/router.js
@@ -1,10 +1,11 @@
 const fs = require('fs');
 const path = require('path');
-const handler = require('./handler');
 const urlParse = require('url');
+const handler = require('./handler');
+const model = require('./model');
 
 module.exports = function(req, res) {
-  var { pathname: url, query } = urlParse.parse(req.url, true);
+  const { pathname: url, query } = urlParse.parse(req.url, true);
   // 将 req.url中解构出来的 query 查询参数，添加为 req 对象的自定义属性，属性名叫做 query
   req.query = query;
   // 获取请求的URL地址
@@ -12,38 +13,83 @@ module.exports = function(req, res) {
   // console.log(url);
 
   // 通过 req.method 获取到 请求方式
-  var method = req.method.toLowerCase();
+  const method = req.method.toLowerCase();
+
+  // 设置跨域访问头
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET, POST, OPTIONS, PUT, DELETE'
+  );
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
+  // 处理 OPTIONS 预检请求
+  if (method === 'options') {
+    res.writeHead(200);
+    res.end();
+    return;
+  }
 
   if (url === '/') {
-    //默认
+    // 默认
     handler.getIndexPage(req, res);
   } else if (url === '/info') {
-    //查看
+    // 查看
     console.log(req);
     handler.showHeroInfo(req, res);
   } else if (url === '/del') {
-    //删除
+    // 删除
     handler.delHeroInfo(req, res);
   } else if (url === '/add' && method === 'get') {
-    //添加
+    // 添加
     handler.addheroPage(req, res);
   } else if (url === '/ajaxPostFile' && method === 'post') {
-    //上传图片
+    // 上传图片
     handler.sendImgFile(req, res);
   } else if (url === '/add' && method === 'post') {
-    //提交英雄数据
+    // 提交英雄数据
     handler.submitData(req, res);
   } else if (url === '/edit' && method === 'get') {
-    //编辑页面
+    // 编辑页面
     handler.getEditHero(req, res);
   } else if (url === '/uploadEditData' && method === 'post') {
     handler.uploadEditHero(req, res);
+  }
+  // API 路由
+  else if (url === '/api/heroes' && method === 'get') {
+    // 获取所有英雄列表 API
+    model.getAllHero((err, heros) => {
+      if (err) {
+        res.writeHead(500, { 'Content-Type': 'application/json' });
+        res.end(JSON.stringify({ error: 'Internal Server Error' }));
+        return;
+      }
+      res.writeHead(200, { 'Content-Type': 'application/json' });
+      res.end(JSON.stringify(heros));
+    });
+  } else if (url === '/api/hero' && method === 'get') {
+    // 获取单个英雄信息 API
+    const { id } = req.query;
+    model.getHeroById(id, (err, hero) => {
+      if (err) {
+        res.writeHead(500, { 'Content-Type': 'application/json' });
+        res.end(JSON.stringify({ error: 'Internal Server Error' }));
+        return;
+      }
+      if (!hero) {
+        res.writeHead(404, { 'Content-Type': 'application/json' });
+        res.end(JSON.stringify({ error: 'Hero not found' }));
+        return;
+      }
+      res.writeHead(200, { 'Content-Type': 'application/json' });
+      res.end(JSON.stringify(hero));
+    });
   } else if (
     url.indexOf('/img/') === 0 ||
     url.indexOf('/node_modules/') === 0
   ) {
     // 处理图片请求和 node_modules 模块的资源文件请求
-    fs.readFile(path.join(__dirname, '../' + url), (err, data) => {
+    fs.readFile(path.join(__dirname, `../${url}`), (err, data) => {
       if (err) {
         res.end('404');
       } else {
